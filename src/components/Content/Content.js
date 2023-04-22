33276a7ba9bcbfd49a6c36cf32aaa01da3da0152
Dmitriy Hoperskiy <Dmitrii.Khoperskii@imr.team>
2023-04-22 15:51:26 +0300

    operation creation and operations list

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -7,6 +7,13 @@ import formatNumber from '../../utils/formatNumber';
 import { IconButton } from '@mui/material';
 import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import FastfoodOutlinedIcon from '@mui/icons-material/FastfoodOutlined';
+import LocalMoviesOutlinedIcon from '@mui/icons-material/LocalMoviesOutlined';
+import CheckroomOutlinedIcon from '@mui/icons-material/CheckroomOutlined';
+import LibraryBooksOutlinedIcon from '@mui/icons-material/LibraryBooksOutlined';
+import HouseOutlinedIcon from '@mui/icons-material/HouseOutlined';
+import DirectionsCarFilledOutlinedIcon from '@mui/icons-material/DirectionsCarFilledOutlined';
+import AddCardOutlinedIcon from '@mui/icons-material/AddCardOutlined';
 
 const AvailablePaper = styled(Paper)({
   padding: '60px 35px',
@@ -34,6 +41,87 @@ const ButtonStyled = styled(IconButton)(({ theme }) => ({
   borderRadius: '8px',
 }));
 
+const OperationContainer = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  alignSelf: 'center',
+  width: '80%',
+  margin: '10px 0',
+  justifyContent: 'space-between',
+  borderBottom: '1px solid ' + theme.palette.headerColor.main,
+  paddingBottom: '20px',
+}));
+
+// const ButtonStyled = styled(IconButton)(({ theme }) => ({
+//   backgroundColor: theme.palette.headerColor.main,
+//   // borderRadius: '8px',
+//   '&:hover': {
+//     backgroundColor: '#fffa00 !important',
+//   },
+// }));
+const getOperationIcon = (operation) => {
+  let icon;
+  switch (operation) {
+    case 'Еда':
+      icon = <FastfoodOutlinedIcon />;
+      break;
+    case 'Развлечения':
+      icon = <LocalMoviesOutlinedIcon />;
+      break;
+    case 'Покупки':
+      icon = <CheckroomOutlinedIcon />;
+      break;
+    case 'Подписки':
+      icon = <LibraryBooksOutlinedIcon />;
+      break;
+    case 'Квартира':
+      icon = <HouseOutlinedIcon />;
+      break;
+    case 'Машина':
+      icon = <DirectionsCarFilledOutlinedIcon />;
+      break;
+    default:
+      icon = <AddCardOutlinedIcon />;
+  }
+  return icon;
+};
+
+const StyledIcon = styled('span')(({ theme }) => ({
+  backgroundColor: theme.palette.headerColor.main,
+  display: 'flex',
+  padding: '12px',
+  marginRight: '15px',
+  borderRadius: '50%',
+}));
+
+const OperationCard = ({ operation }) => {
+  return (
+    <OperationContainer>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <StyledIcon>{getOperationIcon(operation.category)}</StyledIcon>
+        <Typography>
+          {operation.category ? operation.category : 'Доход'}
+        </Typography>
+      </div>
+      <div>
+        {operation.comment && (
+          <Typography sx={{ textAlign: 'end' }}>{operation.comment}</Typography>
+        )}
+        {operation.type === 'income' && (
+          <Typography
+            sx={{ color: '#10ff00', textAlign: 'end' }}
+          >{`+ ${formatNumber(operation.amount)}₽`}</Typography>
+        )}
+        {operation.type === 'expense' && (
+          <Typography
+            sx={{ color: '#ff1313', textAlign: 'end' }}
+          >{`- ${formatNumber(operation.amount)}₽`}</Typography>
+        )}
+      </div>
+    </OperationContainer>
+  );
+};
+
 const Content = ({ operations, setModalOpen, setOperationType }) => {
   const [availableMoney, setAvailableMoney] = React.useState(0);
 
@@ -47,8 +135,6 @@ const Content = ({ operations, setModalOpen, setOperationType }) => {
     setModalOpen(true);
   };
 
-  console.log('operations', operations);
-
   React.useEffect(() => {
     const newAvailable = operations.reduce((acc, val) => {
       if (val.type === 'income') {
@@ -56,13 +142,16 @@ const Content = ({ operations, setModalOpen, setOperationType }) => {
       } else {
         return Number(acc) - Number(val.amount);
       }
-    }, availableMoney);
-    console.log('newAvailable', newAvailable);
+    }, 0);
     setAvailableMoney(newAvailable);
   }, [operations]);
 
+  const operationsMapped = operations.map((el) => (
+    <OperationCard operation={el} />
+  ));
+
   return (
-    <Card>
+    <Card sx={{ boxShadow: 'none' }}>
       <CardContent
         sx={{
           display: 'flex',
@@ -93,6 +182,18 @@ const Content = ({ operations, setModalOpen, setOperationType }) => {
             <Typography sx={{ mt: '5px', fontSize: '14px' }}>Расход</Typography>
           </ButtonWrapper>
         </ButtonsRow>
+        <Typography variant="h3" sx={{ mt: '25px' }}>
+          История операций
+        </Typography>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            marginTop: '20px',
+          }}
+        >
+          {operationsMapped}
+        </div>
       </CardContent>
     </Card>
   );
--- src/components/OperationModal/OperationModal.js
+++ src/components/OperationModal/OperationModal.js
@@ -11,12 +11,36 @@ import {
   FormControl,
   InputAdornment,
   TextField,
+  Typography,
+  styled,
+  IconButton,
 } from '@mui/material';
 import { NumericFormat } from 'react-number-format';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { DesktopDatePicker } from '@mui/x-date-pickers';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import ruLocale from 'date-fns/locale/ru';
+import FastfoodOutlinedIcon from '@mui/icons-material/FastfoodOutlined';
+import LocalMoviesOutlinedIcon from '@mui/icons-material/LocalMoviesOutlined';
+import CheckroomOutlinedIcon from '@mui/icons-material/CheckroomOutlined';
+import LibraryBooksOutlinedIcon from '@mui/icons-material/LibraryBooksOutlined';
+import HouseOutlinedIcon from '@mui/icons-material/HouseOutlined';
+import DirectionsCarFilledOutlinedIcon from '@mui/icons-material/DirectionsCarFilledOutlined';
+
+const ButtonStyled = styled(IconButton)(({ theme }) => ({
+  // backgroundColor: theme.palette.headerColor.main,
+  backgroundColor: theme.palette.background.default,
+  borderRadius: '8px',
+  '&:hover': {
+    backgroundColor: '#fffa00 !important',
+  },
+}));
+
+const ButtonWrapper = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+});
 
 const NumericFormatCustom = React.forwardRef(function NumericFormatCustom(
   props,
@@ -54,10 +78,27 @@ const OperationModal = ({
   const [amountValue, setAmountValue] = React.useState(0);
   const [dateValue, setDateValue] = React.useState(new Date());
   const [commentValue, setCommentValue] = React.useState('');
+  const [categoryName, setCategoryName] = React.useState('');
 
-  const handleClickOpen = () => {
-    setModalOpen(true);
-  };
+  const CategoryButton = ({ label, children }) => (
+    <ButtonWrapper
+      sx={{
+        width: '33%',
+        mb: '10px',
+      }}
+    >
+      <ButtonStyled
+        sx={{
+          backgroundColor:
+            categoryName === label ? theme.palette.headerColor.main : '#fff',
+        }}
+        onClick={() => setCategoryName(label)}
+      >
+        {children}
+      </ButtonStyled>
+      <Typography sx={{ mt: '5px', fontSize: '14px' }}>{label}</Typography>
+    </ButtonWrapper>
+  );
 
   const handleAmountChange = (e) => {
     setAmountValue(e.target.value);
@@ -78,10 +119,13 @@ const OperationModal = ({
       amount: amountValue,
       date: dateValue,
       comment: commentValue,
+      category: categoryName,
     };
     setOperations([...operations, newOperation]);
     setModalOpen(false);
   };
+
+  console.log('categoryName', categoryName);
 
   const handleClose = () => {
     setModalOpen(false);
@@ -102,10 +146,10 @@ const OperationModal = ({
         sx: {
           position: 'absolute',
           right: '-20px',
-          height: '60%',
+          height: '65%',
           maxWidth: '400px',
           padding: '0',
-          backgroundColor: theme.palette.background.default,
+          backgroundColor: '#fff',
         },
       }}
       maxWidth="xs"
@@ -173,6 +217,40 @@ const OperationModal = ({
               variant="standard"
             />
           </FormControl>
+          {operationType === 'expense' && (
+            <div>
+              <Typography sx={{ mt: '20px', fontSize: '16px' }}>
+                Категория:
+              </Typography>
+              <div
+                style={{
+                  display: 'flex',
+                  width: '100%',
+                  flexWrap: 'wrap',
+                  marginTop: '10px',
+                }}
+              >
+                <CategoryButton onClick={() => {}} label="Еда">
+                  <FastfoodOutlinedIcon />
+                </CategoryButton>
+                <CategoryButton onClick={() => {}} label="Развлечения">
+                  <LocalMoviesOutlinedIcon />
+                </CategoryButton>
+                <CategoryButton onClick={() => {}} label="Покупки">
+                  <CheckroomOutlinedIcon />
+                </CategoryButton>
+                <CategoryButton onClick={() => {}} label="Подписки">
+                  <LibraryBooksOutlinedIcon />
+                </CategoryButton>
+                <CategoryButton onClick={() => {}} label="Квартира">
+                  <HouseOutlinedIcon />
+                </CategoryButton>
+                <CategoryButton onClick={() => {}} label="Машина">
+                  <DirectionsCarFilledOutlinedIcon />
+                </CategoryButton>
+              </div>
+            </div>
+          )}
         </DialogContent>
         <DialogActions>
           <Button onClick={handleSave} color="success">
